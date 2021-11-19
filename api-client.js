b788937251981516fdeed5a05f2f7ec28f7e5a68
Sterre Sprengers <sterresprengers@gmail.com>
2021-11-19 13:06:35 +0100

    making backup

--- api-client.js
+++ api-client.js
@@ -8,16 +8,15 @@ const getData = async () => {
             }
         });
         const data = await response.json();
-        // console.log("The data in getData function is ", data);
         return data;
     } catch (error) {
         console.log(error);
-    }
+    };
 };
 
 const postData = async function () {
     const apiUrl = "http://localhost:3000";
-    const newTaskInput = document.querySelector("#new-task-input-field")
+    const newTaskInput = document.querySelector("#new-task-input-field");
     const newTask = {description: `${newTaskInput.value}`, done: false};
     newTaskInput.value= null;
     try {
@@ -27,48 +26,30 @@ const postData = async function () {
             headers: {
                 "Content-Type": "application/json",
             },
-        })
+        });
         const data = await response.json();
-        console.log("The data in postData function is ", data)
-        return data
+        return data;
     } catch (error) {
-        console.log(error)
-    }
-}
+        console.log(error);
+    };
+};
 
-// const deleteData = async (id) => {
-//     console.log("deleteData function was entered");
-//     const apiUrl = `http://localhost:3000/${id}`;
-//     try {
-//         console.log("fetching in deleteData started");
-//         await fetch(apiUrl, {
-//             method: "DELETE",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             }
-//         })
-//         console.log("fetching in deleteData finished")
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-const deleteData = (id) => {
-    console.log("deleteData function was entered");
+const deleteData = async (id) => {
     const apiUrl = `http://localhost:3000/${id}`;
-    console.log("fetching in deleteData started");
-    fetch(apiUrl, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    console.log("fetching in deleteData finished")
-}
-
+    try {
+        await fetch(apiUrl, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    };
+};
 
 const getSingleData = async (id) => {
-    console.log("getSingleData fetch function was entered");
+    // console.log("getSingleData fetch function was entered");
     const apiUrl = `http://localhost:3000/${id}`;
     try {
         const response = await fetch(apiUrl, {
@@ -76,27 +57,22 @@ const getSingleData = async (id) => {
             headers: {
                 "Content-Type": "application/json",
             }
-        })
+        });
         const data = await response.json();
         const dataDone = data.done;
-        console.log("This is the data.done fetched in getSingleData", dataDone);
         return dataDone;
     } catch (error) {
         console.log(error);
-    }
-}
+    };
+};
 
 const putData = async (id, connectedLabel) => {
-    console.log("putData function was entered");
     const apiUrl = `http://localhost:3000/${id}`;
     let dataDone = await getSingleData(id);
-    console.log("This is the returned dataDone by getSingleData in putData function", dataDone)
-    console.log("connectedLabel in putDat is:", connectedLabel)
     if (dataDone == true) {
         try {
             const setToFalse = {done: false};
             connectedLabel.classList.remove("crossed-through")
-            console.log("connectedLabel has changed to:", connectedLabel)
             const response = await fetch(apiUrl , {
                 method: "PUT",
                 body: JSON.stringify(setToFalse),
@@ -105,27 +81,24 @@ const putData = async (id, connectedLabel) => {
                 }
             });
             const data = await response.json();
-            console.log(data, "if dataDone === true")
         } catch (error) {
             console.log(error);
         };
     } 
     if (dataDone == false) {
         try {
-            const setToTrue = {done: true}
-            connectedLabel.classList.add("crossed-through")
-            console.log("connectedLabel has changed to:", connectedLabel)
+            const setToTrue = {done: true};
+            connectedLabel.classList.add("crossed-through");
             const response = await fetch(apiUrl , {
                 method: "PUT",
                 body: JSON.stringify(setToTrue),
                 headers: {
                     "Content-Type": "application/json",
                 }
-            })
+            });
             const data = await response.json();
-            console.log(data, "if dataDone === false")
         } catch (error) {
             console.log(error);
-        }
-    }
-}
+        };
+    };
+};
--- script.js
+++ script.js
@@ -10,70 +10,76 @@ async function awaitGetData () {
         // return dataDone
         // activateBtnCheckbox(data);
     });
-    activateBtnDel();
-    // activateBtnCheckbox();
+    activateBtnDel(fetchedData);
+    activateBtnCheckbox(fetchedData);
 };
 awaitGetData();
-
-const btnAddNewTask = document.querySelector("#btn-add-new-task");
-
-btnAddNewTask.addEventListener("click", async () => {
-    console.log("add new task button was clicked");
-    const data = await postData();
-    console.log("Newly created task-data is:", data);
-    addTaskToList(data);
-    return data;
-});
 
 const addTaskToList = (data) => {
     const toDoList = document.querySelector("#to-do-list");
     const labelElement = document.createElement("label");
     labelElement.classList.add("task-item");
+    // >> delete button:
     const btnDelete = document.createElement("button");
-    const btnDelText = document.createTextNode("Delete"); // to be replaced with trash-icon
-    btnDelete.appendChild(btnDelText); // to be replaced with trash-icon
+    btnDelete.classList.add("btn-del");
     btnDelete.setAttribute("value", data._id);
-    btnDelete.setAttribute("class", "btn-del");
-    // console.log("this is the btn-del:", btnDelete);
+    // console.log("this is the btnDelete", btnDelete)
+    // const btnDelText = document.createTextNode("Delete"); // to be replaced with trash-icon
+    // btnDelete.appendChild(btnDelText); // to be replaced with trash-icon
+    const iElement = document.createElement("i");
+    iElement.setAttribute("class", "fa fa-trash");
+
+    // >> checkbox element:
     const newCheckbox = document.createElement("input");
     newCheckbox.setAttribute("type", "checkbox");
     newCheckbox.setAttribute("class", "btn-checkbox");
     newCheckbox.setAttribute("value", data._id);
     // console.log(newCheckbox);
     const checkboxText = document.createTextNode(data.description);
+    btnDelete.appendChild(iElement);
     labelElement.appendChild(newCheckbox);
     labelElement.appendChild(checkboxText);
     labelElement.appendChild(btnDelete);
     toDoList.appendChild(labelElement);
 };
 
+const btnAddNewTask = document.querySelector("#btn-add-new-task");
+btnAddNewTask.addEventListener("click", async () => {
+    // console.log("add new task button was clicked");
+    const data = await postData();
+    // console.log("Newly created task-data is:", data);
+    addTaskToList(data);
+    return data;
+});
+
 const activateBtnDel = () => {
     const btnDeleteTask = document.querySelectorAll(".btn-del");
     console.log("These are the del-btns:", btnDeleteTask);
     btnDeleteTask.forEach(button => { 
-        button.addEventListener("click", (event) => {
+        button.addEventListener("click", async (event) => {
             console.log("a del-button was clicked");
-            const taskId = event.target.value;
-            console.log("the ID of clicked del-button is:", taskId);
-            deleteData(taskId);
+            if (event.target.className = "btn-del") {
+                const taskId = event.target.value;
+                console.log("the ID of clicked del-button is:", taskId);
+                deleteData(taskId);
+            };
         });
     });
 };
 
-
-// const activateBtnCheckbox = (data) => {
-//     const btnCheckbox = document.querySelectorAll(".btn-checkbox");
-//     console.log("these are the checkbox-btns:", btnCheckbox)
-//     btnCheckbox.forEach(button => {
-//         button.addEventListener("change", (event) => {
-//             // const checkboxText = document.querySelector(`${taskId}`);
-//             // console.log("This is the checkbox selected via change-event", checkboxText)
-//             const taskId = event.target.value;
-//             console.log("the ID of the changed checkbox is:", taskId)
-//             const connectedLabel = event.target.parentNode;
-//             console.log("Label connected to the checkbox is:", connectedLabel)
-//             // event.target.parentNode.classList.add("crossed-through");
-//             putData(taskId, connectedLabel)
-//         })
-//     })
-// }
+const activateBtnCheckbox = (data) => {
+    const btnCheckbox = document.querySelectorAll(".btn-checkbox");
+    // console.log("these are the checkbox-btns:", btnCheckbox);
+    btnCheckbox.forEach(button => {
+        button.addEventListener("change", (event) => {
+            // const checkboxText = document.querySelector(`${taskId}`);
+            // console.log("This is the checkbox selected via change-event", checkboxText)
+            const taskId = event.target.value;
+            // console.log("the ID of the changed checkbox is:", taskId)
+            const connectedLabel = event.target.parentNode;
+            // console.log("Label connected to the checkbox is:", connectedLabel)
+            // event.target.parentNode.classList.add("crossed-through");
+            putData(taskId, connectedLabel);
+        });
+    });
+};
